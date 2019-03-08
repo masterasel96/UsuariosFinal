e79afdab5f36e4db22711be4dbee4af2d3e8e440
Joseda23 <josedam90@gmail.com>
2019-03-08 14:35:14 +0100

    Añado Imports en InstitutoDAO

--- src/java/modelo/InstitutoDAO.java
+++ src/java/modelo/InstitutoDAO.java
@@ -5,9 +5,15 @@
  */
 package modelo;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
- * @author juanjesus
+ * @author josedavid
  */
 public class InstitutoDAO {
     
