561292f4cf37bd2b02088313a1754f9bc7aae76e
masterasel96 <jjtm1996@gmail.com>
2019-03-08 14:21:28 +0100

    creacion usuarioC

--- src/java/controlador/UsuarioC.java
+++ src/java/controlador/UsuarioC.java
@@ -1,14 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import modelo.Usuario;
+import modelo.UsuarioDAO;
+
 
 /**
  *
- * @author juanjesus
+ * @author -----
  */
-public class UsuarioC {
+public class UsuarioC extends HttpServlet {    
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+        response.setContentType("text/html;charset=UTF-8");
+       
+       
+        HttpSession sesion = request.getSession(true);
+        
+        
+        String nombre = request.getParameter("txtNombre");
+        
+        sesion.setAttribute("nombre", nombre);
+        
+       
+        
+        String apellidos = request.getParameter("txtApellidos");
+        sesion.setAttribute("apellidos", apellidos);
+        
+        int edad = Integer.parseInt(request.getParameter("txtEdad"));
+        
+
+        sesion.setAttribute("edad", edad);
+        
+        int idInsti = Integer.parseInt(request.getParameter("txtIdInsti"));
+        
+
+        sesion.setAttribute("edad", edad);
+
+       
+
+            UsuarioDAO.insertarUsuario(nombre,apellidos,edad,idInsti);
+        
+
+      
+         ArrayList<Usuario> lista = UsuarioDAO.consultarUsuarios();
+        
+        
+        lista.forEach(System.out::println);
+        
+        
+        response.sendRedirect(response.encodeRedirectURL("index.jsp"));
+
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
     
 }
