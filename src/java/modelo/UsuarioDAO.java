7aef5177dbb6be16fc5a6ff0140f100d938c2180
masterasel96 <jjtm1996@gmail.com>
2019-03-08 14:23:57 +0100

    creacion usuarioDAO

--- src/java/modelo/UsuarioDAO.java
+++ src/java/modelo/UsuarioDAO.java
@@ -1,14 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
- *
- * @author juanjesus
+ * @author 
  */
 public class UsuarioDAO {
     
+    
+    public static ArrayList<Usuario> consultarUsuarios(){
+        Statement st;
+        ResultSet res;
+        ArrayList<Usuario> lista = new ArrayList();
+        
+        
+        String sql = "select * from usuarios";
+  
+        Conexion conexion = new Conexion();
+        
+        try {
+            
+            st = conexion.getConexion().createStatement(); 
+            
+            res = st.executeQuery(sql);
+            while (res.next()){
+                Usuario j = new Usuario();
+                j.setNombre(res.getString("nombre"));
+                j.setApellidos(res.getString("apellidos"));
+                j.setEdad(res.getInt("edad"));
+                j.setId(res.getInt("id_insti"));
+               
+
+                lista.add(j);
+            }
+            st.close();
+            conexion.cerrarConexion();
+        } catch (SQLException e) {
+            System.out.println("Problemas durante la consulta en tabla usuarios");
+            System.out.println(e);
+            
+        }
+
+        return lista;  
+    }
+    
+    public static int insertarUsuario(String nombre,String apellidos, int edad, int id_insti){
+        
+        String sql = "insert into usuarios(nombre, apellidos, edad, id_insti) values ('"+nombre+"','"+apellidos+"','"+edad+"','"+id_insti+"')";
+        Conexion conexion = new Conexion();
+        
+        PreparedStatement prest;
+
+        try { 
+            prest = conexion.getConexion().prepareStatement(sql);
+
+
+
+      
+            int nfilas = prest.executeUpdate();
+    
+           
+            prest.close();
+            
+           
+            conexion.cerrarConexion();
+            
+            return nfilas;
+        } catch (SQLException e) {
+            System.out.println("Problemas durante la inserción de datos en la tabla Jugadores");
+            System.out.println(e);
+            return -1;
+        }
+    }
+    
+   
+    
 }
+
