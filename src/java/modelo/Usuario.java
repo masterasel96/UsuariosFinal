d1f9d389fbdaf68cc10b93074b90f632858b90e9
masterasel96 <jjtm1996@gmail.com>
2019-03-08 14:28:18 +0100

    creacion Usuario

--- src/java/modelo/Usuario.java
+++ src/java/modelo/Usuario.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
 /**
  *
- * @author juanjesus
+ * @author J. Carlos F. Vico <jfervic933@maralboran.es>
  */
 public class Usuario {
     
+    private int id_insti;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    
+    public int getId() {
+        return id_insti;
+    }
+
+    public void setId(int id) {
+        this.id_insti = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    
+
+    
+    
+
+   
 }
+
