70ed429372711d93bb23b66684a7ffd69e14de59
masterasel96 <jjtm1996@gmail.com>
2019-03-08 14:28:56 +0100

    creacion archivo de conexion

--- src/java/modelo/Conexion.java
+++ src/java/modelo/Conexion.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
-
 /**
  *
- * @author juanjesus
+ * @author J. Carlos F. Vico <jfervic933@maralboran.es>
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
     
+    private static final String SERVIDOR = "jdbc:mysql://localhost/";
+    private static final String NOMBRE_BASE_DATOS = "usuarios";
+    private static final String USER = "xema";
+    private static final String PASS = "123";
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    
+    private Connection conexion;
+    
+    public Conexion(){
+        
+        try {
+            Class.forName(DRIVER); 
+
+            
+            conexion = DriverManager.getConnection(SERVIDOR + NOMBRE_BASE_DATOS, USER, PASS);
+
+            System.out.println("Conexión realizada con éxito.");
+
+        } catch (SQLException | ClassNotFoundException e) {
+           
+            System.out.println(e);
+        }
+    }
+    
+    public Connection getConexion() {
+        return conexion;
+    }
+    
+    public void cerrarConexion() {
+        try {
+            conexion.close();
+            System.out.println("Cerrando la conexión con la BD.");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        conexion = null;
+    }
+    
 }
+
+
